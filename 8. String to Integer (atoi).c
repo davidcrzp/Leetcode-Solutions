f21cedef22a12d6c9269ad4c5f5284ba5076a318
Davidcrzp <davidcrzpz@gmail.com>
2026-03-19 21:43:00 -0500

    Removed duplicates

--- 8. String to Integer (atoi).c
+++
@@ -1,9 +0,0 @@
-int myAtoi(char* s) {
-    long l = strtol(s, NULL, 10);
-    if(l > INT_MAX) {
-        return INT_MAX;
-    } else if(l < INT_MIN) {
-        return INT_MIN;
-    }
-    return (int)l;
-}
