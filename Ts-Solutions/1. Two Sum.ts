09bf2c6bbcb698f9bcccb738fc4d834c1143bb10
Davidcrzp <davidcrzpz@gmail.com>
2026-03-19 21:45:16 -0500

    Create 1. Two Sum.ts

---
+++ Ts-Solutions/1. Two Sum.ts
@@ -0,0 +1,14 @@
+function twoSum(nums: number[], target: number): number[] {
+    let arr: number[] = [];
+    
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i + 1; j < nums.length; j++) {
+            if(nums[i] + nums[j] == target) {
+                arr.push(i, j);
+                return arr;
+            }
+        }
+    }
+
+    return arr;
+};
